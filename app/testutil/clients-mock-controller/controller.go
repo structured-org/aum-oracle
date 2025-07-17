d30be7c4bc3bfa3f1b4ce5c6130cfe1aa8c11912
sotnikov-s <ssotnikov93@gmail.com>
2025-07-17 13:52:20 +0300

    add GET methods to mock controller

--- app/testutil/clients-mock-controller/controller.go
+++ app/testutil/clients-mock-controller/controller.go
@@ -63,197 +63,216 @@ func (cm *ClientsMockController) Start(port int) error {
 }
 
 func (cm *ClientsMockController) handleBinanceUmPositions(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusBadRequest)
-		return
-	}
-	var positions []*binanceportfolio.UMPosition
-	if err := json.Unmarshal(body, &positions); err != nil {
-		http.Error(w, "Error unmarshaling request body", http.StatusBadRequest)
-		return
-	}
-	cm.binanceMockClient.SetUmPositions(positions)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "Binance UM Positions updated successfully")
-}
+	switch r.Method {
+	case http.MethodPost:
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		var positions []*binanceportfolio.UMPosition
+		if err := json.Unmarshal(body, &positions); err != nil {
+			http.Error(w, "Error unmarshaling request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		cm.binanceMockClient.SetUmPositions(positions)
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "Binance UM Positions updated successfully")
 
-func (cm *ClientsMockController) handleBinanceCmPositions(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
-		return
+	case http.MethodGet:
+		positions, _ := cm.binanceMockClient.GetUmPositions(r.Context())
+		json.NewEncoder(w).Encode(positions)
+	default:
+		http.Error(w, "Only POST and GET methods are allowed", http.StatusMethodNotAllowed)
 	}
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusBadRequest)
-		return
-	}
-	var positions []*binanceportfolio.CMPosition
-	if err := json.Unmarshal(body, &positions); err != nil {
-		http.Error(w, "Error unmarshaling request body", http.StatusBadRequest)
-		return
-	}
-	cm.binanceMockClient.SetCmPositions(positions)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "Binance CM Positions updated successfully")
 }
 
 func (cm *ClientsMockController) handleBinancePMAccountInfo(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
-		return
+	switch r.Method {
+	case http.MethodPost:
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		var account binanceportfolio.Account
+		if err := json.Unmarshal(body, &account); err != nil {
+			http.Error(w, "Error unmarshaling request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		cm.binanceMockClient.SetPMAccountInfo(&account)
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "Binance PM Account Info updated successfully")
+	case http.MethodGet:
+		account, _ := cm.binanceMockClient.GetPMAccountInfo(r.Context())
+		json.NewEncoder(w).Encode(account)
+	default:
+		http.Error(w, "Only POST and GET methods are allowed", http.StatusMethodNotAllowed)
 	}
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusBadRequest)
-		return
-	}
-	var account binanceportfolio.Account
-	if err := json.Unmarshal(body, &account); err != nil {
-		http.Error(w, "Error unmarshaling request body", http.StatusBadRequest)
-		return
-	}
-	cm.binanceMockClient.SetPMAccountInfo(&account)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "Binance PM Account Info updated successfully")
 }
 
 func (cm *ClientsMockController) handleBinancePMAccountBalance(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
-		return
+	switch r.Method {
+	case http.MethodPost:
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		var balances []*binanceportfolio.Balance
+		if err := json.Unmarshal(body, &balances); err != nil {
+			http.Error(w, "Error unmarshaling request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		cm.binanceMockClient.SetPMAccountBalance(balances)
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "Binance PM Account Balance updated successfully")
+	case http.MethodGet:
+		balances, _ := cm.binanceMockClient.GetPMAccountBalance(r.Context())
+		json.NewEncoder(w).Encode(balances)
+	default:
+		http.Error(w, "Only POST and GET methods are allowed", http.StatusMethodNotAllowed)
 	}
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusBadRequest)
-		return
-	}
-	var balances []*binanceportfolio.Balance
-	if err := json.Unmarshal(body, &balances); err != nil {
-		http.Error(w, "Error unmarshaling request body", http.StatusBadRequest)
-		return
-	}
-	cm.binanceMockClient.SetPMAccountBalance(balances)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "Binance PM Account Balance updated successfully")
 }
 
 func (cm *ClientsMockController) handleBinanceSpotAccountInfo(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
-		return
+	switch r.Method {
+	case http.MethodPost:
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		var account binance.Account
+		if err := json.Unmarshal(body, &account); err != nil {
+			http.Error(w, "Error unmarshaling request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		cm.binanceMockClient.SetSpotAccountInfo(&account)
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "Binance Spot Account Info updated successfully")
+	case http.MethodGet:
+		account, _ := cm.binanceMockClient.GetSpotAccountInfo(r.Context())
+		json.NewEncoder(w).Encode(account)
+	default:
+		http.Error(w, "Only POST and GET methods are allowed", http.StatusMethodNotAllowed)
 	}
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusBadRequest)
-		return
-	}
-	var account binance.Account
-	if err := json.Unmarshal(body, &account); err != nil {
-		http.Error(w, "Error unmarshaling request body", http.StatusBadRequest)
-		return
-	}
-	cm.binanceMockClient.SetSpotAccountInfo(&account)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "Binance Spot Account Info updated successfully")
 }
 
 func (cm *ClientsMockController) handleJupiterCustodyInfo(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
-		return
+	switch r.Method {
+	case http.MethodPost:
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		var req struct {
+			PublicKey string                                   `json:"publicKey"`
+			Data      jupiterclient.JupiterPerpsCustodyAccount `json:"data"`
+		}
+		if err := json.Unmarshal(body, &req); err != nil {
+			http.Error(w, "Error unmarshaling request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		pubKey, err := solana.PublicKeyFromBase58(req.PublicKey)
+		if err != nil {
+			http.Error(w, "Invalid public key", http.StatusBadRequest)
+			return
+		}
+		cm.jupiterMockClient.SetJupiterCustodyInfo(pubKey, &req.Data)
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "Jupiter Custody Info updated successfully")
+	case http.MethodGet:
+		account, _ := cm.jupiterMockClient.GetJupiterCustodyInfo(
+			r.Context(),
+			solana.MustPublicKeyFromBase58(r.URL.Query().Get("custody")),
+		)
+		json.NewEncoder(w).Encode(account)
+	default:
+		http.Error(w, "Only POST and GET methods are allowed", http.StatusMethodNotAllowed)
 	}
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusBadRequest)
-		return
-	}
-	var req struct {
-		PublicKey string                                   `json:"publicKey"`
-		Data      jupiterclient.JupiterPerpsCustodyAccount `json:"data"`
-	}
-	if err := json.Unmarshal(body, &req); err != nil {
-		http.Error(w, "Error unmarshaling request body", http.StatusBadRequest)
-		return
-	}
-	pubKey, err := solana.PublicKeyFromBase58(req.PublicKey)
-	if err != nil {
-		http.Error(w, "Invalid public key", http.StatusBadRequest)
-		return
-	}
-	cm.jupiterMockClient.SetJupiterCustodyInfo(pubKey, &req.Data)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "Jupiter Custody Info updated successfully")
 }
 
 func (cm *ClientsMockController) handleJupiterPoolInfo(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
-		return
+	switch r.Method {
+	case http.MethodPost:
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		var poolInfo jupiterclient.JupiterPoolAccount
+		if err := json.Unmarshal(body, &poolInfo); err != nil {
+			http.Error(w, "Error unmarshaling request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		cm.jupiterMockClient.SetJupiterPoolInfo(&poolInfo)
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "Jupiter Pool Info updated successfully")
+	case http.MethodGet:
+		poolInfo, _ := cm.jupiterMockClient.GetJupiterPoolInfo(r.Context(), solana.PublicKey{})
+		json.NewEncoder(w).Encode(poolInfo)
+	default:
+		http.Error(w, "Only POST and GET methods are allowed", http.StatusMethodNotAllowed)
 	}
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusBadRequest)
-		return
-	}
-	var poolInfo jupiterclient.JupiterPoolAccount
-	if err := json.Unmarshal(body, &poolInfo); err != nil {
-		http.Error(w, "Error unmarshaling request body", http.StatusBadRequest)
-		return
-	}
-	cm.jupiterMockClient.SetJupiterPoolInfo(&poolInfo)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "Jupiter Pool Info updated successfully")
 }
 
 func (cm *ClientsMockController) handleSolanaTokenSupply(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
-		return
+	switch r.Method {
+	case http.MethodPost:
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		var tokenSupply solanarpc.UiTokenAmount
+		if err := json.Unmarshal(body, &tokenSupply); err != nil {
+			http.Error(w, "Error unmarshaling request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		cm.solanaMockClient.SetTokenSupply(&tokenSupply)
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "Solana Token Supply updated successfully")
+	case http.MethodGet:
+		tokenSupply, _ := cm.solanaMockClient.GetTokenSupply(r.Context(), solana.PublicKey{})
+		json.NewEncoder(w).Encode(tokenSupply)
+	default:
+		http.Error(w, "Only POST and GET methods are allowed", http.StatusMethodNotAllowed)
 	}
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusBadRequest)
-		return
-	}
-	var tokenSupply solanarpc.UiTokenAmount
-	if err := json.Unmarshal(body, &tokenSupply); err != nil {
-		http.Error(w, "Error unmarshaling request body", http.StatusBadRequest)
-		return
-	}
-	cm.solanaMockClient.SetTokenSupply(&tokenSupply)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "Solana Token Supply updated successfully")
 }
-
 func (cm *ClientsMockController) handleSolanaTokenAccountBalance(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
-		return
+	switch r.Method {
+	case http.MethodPost:
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		var req struct {
+			PublicKey string                  `json:"publicKey"` // This would be the token's public key
+			Data      solanarpc.UiTokenAmount `json:"data"`
+		}
+		if err := json.Unmarshal(body, &req); err != nil {
+			http.Error(w, "Error unmarshaling request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		pubKey, err := solana.PublicKeyFromBase58(req.PublicKey)
+		if err != nil {
+			http.Error(w, "Invalid public key", http.StatusBadRequest)
+			return
+		}
+		cm.solanaMockClient.SetTokenAccountBalance(pubKey, &req.Data)
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "Solana Token Account Balance updated successfully")
+	case http.MethodGet:
+		tokenAccountBalance, _ := cm.solanaMockClient.GetTokenAccountBalance(
+			r.Context(),
+			solana.MustPublicKeyFromBase58(r.URL.Query().Get("token")),
+			solana.PublicKey{},
+		)
+		json.NewEncoder(w).Encode(tokenAccountBalance)
+	default:
+		http.Error(w, "Only POST and GET methods are allowed", http.StatusMethodNotAllowed)
 	}
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusBadRequest)
-		return
-	}
-	var req struct {
-		PublicKey string                  `json:"publicKey"` // This would be the token's public key
-		Data      solanarpc.UiTokenAmount `json:"data"`
-	}
-	if err := json.Unmarshal(body, &req); err != nil {
-		http.Error(w, "Error unmarshaling request body", http.StatusBadRequest)
-		return
-	}
-	pubKey, err := solana.PublicKeyFromBase58(req.PublicKey)
-	if err != nil {
-		http.Error(w, "Invalid public key", http.StatusBadRequest)
-		return
-	}
-	cm.solanaMockClient.SetTokenAccountBalance(pubKey, &req.Data)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "Solana Token Account Balance updated successfully")
 }
